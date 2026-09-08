44fc329ae991f5296473149ac438aa0838b5e456
dhurga <dhurga3102007@gmail.com>
2026-09-08 12:38:51 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,9 +1,7 @@
 DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
-
--- Create Course table
-
--- Insert three records
-
--- Display structure
+CREATE DATABASE dhurga;
+USE dhurga;
+create table Course(CourseID int(5)primary key, CourseName varchar(30), Credits int(5), DepartmentID int(5));
+insert into Course values(101,'bca',5,04);
+desc Course;
+select*from Course;
