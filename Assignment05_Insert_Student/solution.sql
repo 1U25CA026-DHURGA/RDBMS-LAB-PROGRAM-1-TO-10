c8f3e201b969f04ddf9ad050902449824c72cefd
dhurga <dhurga3102007@gmail.com>
2026-09-09 09:08:57 +0530

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -1,23 +1,15 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
+
+CREATE DATABASE dhurga;
+USE dhurga;
 
 CREATE TABLE Student(
     StudentID INT(5) PRIMARY KEY,
     StudentName VARCHAR(20) NOT NULL,
-    DOB DATE,
     Gender VARCHAR(10),
     DepartmentID INT(5),
-    Email VARCHAR(30),
-    PhoneNumber BIGINT
 );
-
--- Insert
-
--- 1001 Arun
-
--- 1002 Divya
-
--- 1003 Karthik
-
--- Display all records
+insert into Student values(1001,'Arun','male',101);
+insert into Student values(1002,'Divya','female',102);
+insert into Student values(1003,'Karthik','male',101);
+desc Student;
+select*from Student;
