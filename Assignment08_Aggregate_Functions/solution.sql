7e0166f8ae4f127014b3e5e0110eb1431908ee18
dhurga <dhurga3102007@gmail.com>
2026-09-11 08:57:06 +0530

    Update solution.sql

--- Assignment08_Aggregate_Functions/solution.sql
+++ Assignment08_Aggregate_Functions/solution.sql
@@ -1,15 +1,31 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
 
--- Create Employee table
+CREATE DATABASE dhurga;
+USE dhurga;
+CREATE TABLE Employee (
+    EmployeeID NUMBER(5),
+    EmployeeName VARCHAR2(30),
+    Department VARCHAR2(20),
+    Salary NUMBER(10)
+);
 
--- Insert records
+INSERT INTO Employee VALUES (101, 'Ravi', 'HR', 25000);
+INSERT INTO Employee VALUES (102, 'Meena', 'IT', 40000);
+INSERT INTO Employee VALUES (103, 'Kumar', 'Finance', 35000);
+INSERT INTO Employee VALUES (104, 'Suresh', 'IT', 45000);
+INSERT INTO Employee VALUES (105, 'Latha', 'HR', 30000);
 
--- COUNT()
+-- COUNT
+SELECT COUNT(Salary) AS Total_Employees
+FROM Employee;
 
--- MAX()
+-- MAX
+SELECT MAX(Salary) AS Maximum_Salary
+FROM Employee;
 
--- MIN()
+-- MIN
+SELECT MIN(Salary) AS Minimum_Salary
+FROM Employee;
 
--- AVG()
+-- AVG
+SELECT AVG(Salary) AS Average_Salary
+FROM Employee;
