5e716a3f512f5b109f4fb1bf3966c51dc99e1407
dhurga <dhurga3102007@gmail.com>
2026-09-11 06:41:59 +0530

    Update solution.sql

--- Assignment07_Marksheet/solution.sql
+++ Assignment07_Marksheet/solution.sql
@@ -1,10 +1,22 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
--- Create Marksheet table
+create database dhurga;
+use dhurga;
+CREATE TABLE Marksheet (
+    RollNo NUMBER(5),
+    Name VARCHAR2(30),
+    Department VARCHAR2(10),
+    Marks NUMBER(5)
+);
 
--- Insert sample records
+INSERT INTO Marksheet VALUES (1, 'Arun', 'CSE', 85);
+INSERT INTO Marksheet VALUES (2, 'Divya', 'IT', 78);
+INSERT INTO Marksheet VALUES (3, 'Karthik', 'CSE', 92);
+INSERT INTO Marksheet VALUES (4, 'Nisha', 'ECE', 67);
+INSERT INTO Marksheet VALUES (5, 'Rahul', 'IT', 88);
 
--- Display students with Marks > 80
+-- Students with marks greater than 80
+-- Sorted in descending order
 
--- Sort by Marks DESC
+SELECT *
+FROM Marksheet
+WHERE Marks > 80
+ORDER BY Marks DESC;
